f383cb353b1ee3e62ad8c6b205d47241106a4a1c
JamesDalboth <james.dalboth@gmail.com>
2019-12-16 15:28:05 +0000

    Updates

---
+++ precache-manifest.0cee00a0dabb45da6f04b3ed47741744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5733bd918f34f1e3d46182e99e2ca82b",
+    "url": "/MosaiclyOnline/index.html"
+  },
+  {
+    "revision": "a02875ed491b9a5bf785",
+    "url": "/MosaiclyOnline/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afcb36262b1291e4979e",
+    "url": "/MosaiclyOnline/static/js/2.c316ce37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MosaiclyOnline/static/js/2.c316ce37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a02875ed491b9a5bf785",
+    "url": "/MosaiclyOnline/static/js/main.6c6c2786.chunk.js"
+  },
+  {
+    "revision": "43964c284236d43a1411",
+    "url": "/MosaiclyOnline/static/js/runtime-main.df034b8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MosaiclyOnline/static/media/logo.5d5d9eef.svg"
+  }
+]);
